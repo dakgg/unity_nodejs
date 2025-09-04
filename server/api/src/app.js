3b7e2111917b507e2a121f09ee1f51fb078e301a
hs.hwang <hs.hwang@smartstudygames.com>
2025-09-04 18:05:14 +0900

    redis 추가중...

--- server/api/src/app.js
+++ server/api/src/app.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello Express!");
 });
 
+app.get('/ping', (req, res) => {
+    res.send('Pong');
+});
+
 app.listen(PORT, () => {
   console.log(`서버 실행 중 → http://localhost:${PORT}`);
 });
